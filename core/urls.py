baf5f8b39acdde86897c8ea5ffdd6227a0712c4c
Tiago Dória <tiagodoriap@gmail.com>
2019-04-14 20:00:15 -0300

    inserindo rotas atualizadas

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,4 @@ from . import  views
 urlpatterns = [
     # Examples:
     url(r'^$', views.home, name='home'),
-    url(r'^banca_pendente/$', views.banca_pendente, name='banca_pendente'),
-    url(r'^agendamentos_pendentes/$', views.agendamento_pendente, name='agendamentos_pendentes'),
-    url(r'^defesas_confirmadas/$', views.defesas_confirmadas, name='defesas_confirmadas'),
 ]
