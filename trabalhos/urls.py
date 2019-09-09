bca9da4415d75efb144ebbab7058c81f366560c1
Tiago Dória <tiagodoriap@gmail.com>
2019-09-08 21:33:10 -0300

    bancas

--- trabalhos/urls.py
+++ trabalhos/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^download_relatorio_quantidade/$', views.html_to_pdf_view_quantidade, name='download_relatorio_quantidade'),
     url(r'^relatorio_orientador/(?P<pk>\d+)/$', views.relatorio_orientador, name='relatorio_orientador'),
     url(r'^confirmar_defesa/(?P<pk>\d+)/$', views.confirmar_defesa, name='confirmar_defesa'),
+    url(r'^lista_bancas/$', views.lista_bancas, name='lista_bancas'),
     
 ]
